d20dc96148aba3d28b8fc25af73ce0cd744e20bb
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-05-29 11:03:14 +0100

    add palindrome class

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace Palindrome
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        foreach (string arg in args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{arg} -> {IsPalindrome(arg)}");
         }
+    }
+
+    static bool IsPalindrome(string s)
+    {
+        if (s == null)
+        {
+            throw new ArgumentNullException(nameof(s));
+        }
+
+        if (s.Length < 2)
+        {
+            return true;
+        }
+
+        return IsPalindromeInternal(s);
+    }
+
+    static bool IsPalindromeInternal(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left]!= s[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
