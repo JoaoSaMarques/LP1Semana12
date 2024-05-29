a681f645b5eeb2282b001012fe925364115d17de
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-05-29 11:07:25 +0100

    GetMinMax1

---
+++ MultipleStuff/Program copy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultipleStuff
+{
+    class SuperList : List<double>
+    {
+        public void GetMinMax1(out double min, out double max)
+        {
+            min = this.Min();
+            max = this.Max();
+        }
+    }
+}
