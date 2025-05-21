c7434e10d921b03152919b01a5b88106813602d2
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-21 14:45:51 +0100

    copy Guarda3

---
+++ JustLikeACollection/Guarda3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JustLikeACollection
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T item0;
+        private T item1;
+        private T item2;
+
+        public Guarda3()
+        {
+            item0 = default(T);
+            item1 = default(T);
+            item2 = default(T);
+        }
+
+        public T GetItem(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return item0;
+                case 1:
+                    return item1;
+                case 2:
+                    return item2;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void SetItem(int i, T item)
+        {
+            switch (i)
+            {
+                case 0:
+                    item0 = item;
+                    break;
+                case 1:
+                    item1 = item;
+                    break;
+                case 2:
+                    item2 = item;
+                    break;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return item0;
+            yield return item1;
+            yield return item2;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
