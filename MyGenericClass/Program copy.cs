8912ac5301a5d611ea057a934276c997c0c96166
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-21 14:31:55 +0100

    empty constructor that initializes 3 vars

--- MyGenericClass/Program copy.cs
+++ MyGenericClass/Program copy.cs
@@ -7,5 +7,12 @@ namespace MyGenericClass
         private T item0;
         private T item1;
         private T item2;
+
+        public Guarda3()
+        {
+            item0 = default(T);
+            item1 = default(T);
+            item2 = default(T);
+        }
     }
 }
