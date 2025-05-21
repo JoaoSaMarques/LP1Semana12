2e5da17e0837001704abe56ca2b6830a6fb3adc2
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-21 14:25:48 +0100

    add foreach

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -5,9 +5,16 @@ namespace TheyCameBefore
 {
     static class BeforeUtils
     {
-        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T item) where T : struct, IComparable<T>
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items,
+        T item) where T : struct, IComparable<T>
         {
-            
+            foreach (var currentItem in items)
+            {
+                if (currentItem.CompareTo(item) < 0)
+                {
+                    yield return currentItem;
+                }
+            }   
         }
     }
 }
